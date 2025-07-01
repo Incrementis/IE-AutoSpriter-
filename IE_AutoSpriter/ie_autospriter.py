3065abbdab923d9abd8ec64ab10c9b4ca1831ef7
Incrementis <incrementis@gmail.com>
2025-07-01 13:07:31 +0200

    Renders by pressing the button (issue #8).
    This implements all the logic contained in the concept (issue #1).
    The main purpose is to improve the existing add-on (e.g., doing tests and improve performance).

--- IE_AutoSpriter/ie_autospriter.py
+++ IE_AutoSpriter/ie_autospriter.py
@@ -4,6 +4,8 @@
 # https://docs.blender.org/api/current/bpy.types.Operator.html
 # https://developer.blender.org/docs/release_notes/2.80/python_api/addons/#Naming
 # https://docs.blender.org/api/current/bpy.props.html#bpy.props.PointerProperty
+# https://docs.blender.org/api/current/bpy.ops.object.html#bpy.ops.object.select_all
+# https://docs.blender.org/api/current/bpy.ops.render.html#bpy.ops.render.render
 # (PT   = Panel Type)
 # (OT   = Operator Type)
 # (PGT  = Property Group Type)
@@ -11,6 +13,8 @@ from bpy.types import Panel
 from bpy.types import PropertyGroup
 from bpy.types import Operator
 import bpy
+import os
+import math
 
 
 # --------
@@ -21,7 +25,7 @@ import bpy
 bl_info = {
     "name": "IE AutoSpriter",
     "author": "Incrementis",
-    "version": (0, 6, 4),
+    "version": (0, 7, 0),
     "blender": (4, 0, 0),
     "location": "View3d > Tool",
     "support": "https://github.com/Incrementis/IE-AutoSpriter-",
@@ -46,6 +50,11 @@ class IEAS_PGT_Inputs(PropertyGroup):
     Save_at:        bpy.props.StringProperty(name="Save at",subtype='FILE_PATH') # File-opener
     Prefix:         bpy.props.StringProperty(name="Prefix")
     Resref:         bpy.props.StringProperty(name="Resref")
+    Object_List:    bpy.props.PointerProperty(
+                                                type=bpy.types.Object,
+                                                name="Object List", # Label for the UI element
+                                                description="Select the armature to be used to apply rendering." # Tooltip
+                                                )
     Resolution_X:   bpy.props.IntProperty(name="Resolution X",default=256, min=1)
     Resolution_Y:   bpy.props.IntProperty(name="Resolution Y",default=256, min=1)
     Every_X_Frame:  bpy.props.IntProperty(name="Every X Frame", default=1, min=1)
@@ -85,8 +94,8 @@ class IEAS_PGT_Inputs(PropertyGroup):
     Cast:       bpy.props.StringProperty(name="CA", default="cast")
     Death:      bpy.props.StringProperty(name="DE", default="death")
     Get_Hit:    bpy.props.StringProperty(name="GH", default="get hit")
-    Get_Up:     bpy.props.StringProperty(name="GU", default="Get up")
-    Ready:      bpy.props.StringProperty(name="SC", default="Ready")
+    Get_Up:     bpy.props.StringProperty(name="GU", default="get up")
+    Ready:      bpy.props.StringProperty(name="SC", default="ready")
     Idle:       bpy.props.StringProperty(name="SD", default="idle")
     Sleep:      bpy.props.StringProperty(name="SL", default="sleep")
     Conjure:    bpy.props.StringProperty(name="SP", default="conjure")
@@ -170,14 +179,153 @@ class IEAS_OT_Final(Operator):
     bl_label = "RENDER"
     
     # Blender specific function which is executed in this case when RENDER button is pressed
+    # TODO: Cleanup/refactor function
     def execute(self, context):
         # Put your render property code here
         print("--------IEAS_OT_Final----------")
         
-        # TODO:
-        # 1. Set path to store the sprites
-        # 2. 
+        pathSaveAt = context.scene.IEAS_properties.Save_at
+        # TODO: Delte print
+        print("pathSaveAt:",pathSaveAt)
         
+        # Just to make sure that the path is formated es expected
+        # (maybe this is not needed,but the "icon viewer" prints a different output than the "Toggle System Console")
+        # Also preventing relative path (user given)
+        pathSaveAt = os.path.abspath(pathSaveAt)
+        # TODO: Delte print
+        print("pathSaveAt:",pathSaveAt)
+        
+        prefixResref = context.scene.IEAS_properties.Prefix + context.scene.IEAS_properties.Resref
+        # TODO: Delte print
+        print("prefixResref:",prefixResref)
+        
+        #selected_objects = bpy.context.selected_objects
+        # TODO: Delte print
+        #print("selected_objects",selected_objects)
+        
+        # deselect all in scene(preventing a second unintended rendering by the user)
+        bpy.ops.object.select_all(action='DESELECT') 
+        
+        #TODO: Loop through all the stored objects
+        # TODO: Delte print
+        print("Object List:",context.scene.IEAS_properties.Object_List.name)
+        objectName = context.scene.IEAS_properties.Object_List.name
+        # Selects the specific object.
+        bpy.context.scene.objects[objectName].select_set(True)
+        
+        # ---- Camera
+        cameraPosFolderNames = {
+            'south': context.scene.IEAS_properties.South,  'south_west': context.scene.IEAS_properties.South_West,
+            'west': context.scene.IEAS_properties.West,    'north_west': context.scene.IEAS_properties.North_West,
+            'north': context.scene.IEAS_properties.North,  'north_east': context.scene.IEAS_properties.North_East,
+            'east': context.scene.IEAS_properties.East,    'south_east': context.scene.IEAS_properties.South_East
+        }
+        cameraPosToggles = {
+            'south': context.scene.IEAS_properties.Use_S,  'south_west': context.scene.IEAS_properties.Use_SW,
+            'west': context.scene.IEAS_properties.Use_W,   'north_west': context.scene.IEAS_properties.Use_NW,
+            'north': context.scene.IEAS_properties.Use_N,  'north_east': context.scene.IEAS_properties.Use_NE,
+            'east': context.scene.IEAS_properties.Use_E,   'south_east': context.scene.IEAS_properties.Use_SE
+        }
+        cameraAngles = {
+            'south': 0,  'south_west': 315,
+            'west': 270,   'north_west': 225,
+            'north': 180,  'north_east': 135,
+            'east': 90,   'south_east': 45
+        }
+        # ----- Animation
+        animationFolderNames = {
+            'A1': context.scene.IEAS_properties.Attack1,    'A2': context.scene.IEAS_properties.Attack2,
+            'A3': context.scene.IEAS_properties.Attack3,    'A4': context.scene.IEAS_properties.Attack4,
+            'CA': context.scene.IEAS_properties.Cast,       'DE': context.scene.IEAS_properties.Death,
+            'GH': context.scene.IEAS_properties.Get_Hit,    'GU': context.scene.IEAS_properties.Get_Up,
+            'SC': context.scene.IEAS_properties.Ready,      'SD': context.scene.IEAS_properties.Idle,
+            'SL': context.scene.IEAS_properties.Sleep,      'SP': context.scene.IEAS_properties.Conjure,
+            'TW': context.scene.IEAS_properties.Dead,       'WK': context.scene.IEAS_properties.Walk,            
+        }
+        animationToggle = {
+            'A1': context.scene.IEAS_properties.Use_A1,     'A2': context.scene.IEAS_properties.Use_A2,
+            'A3': context.scene.IEAS_properties.Use_A3,     'A4': context.scene.IEAS_properties.Use_A4,
+            'CA': context.scene.IEAS_properties.Use_CA,     'DE': context.scene.IEAS_properties.Use_DE,
+            'GH': context.scene.IEAS_properties.Use_GH,     'GU': context.scene.IEAS_properties.Use_GU,
+            'SC': context.scene.IEAS_properties.Use_SC,     'SD': context.scene.IEAS_properties.Use_SD,
+            'SL': context.scene.IEAS_properties.Use_SL,     'SP': context.scene.IEAS_properties.Use_SP,
+            'TW': context.scene.IEAS_properties.Use_TW,     'WK': context.scene.IEAS_properties.Use_WK,            
+        }
+        
+        
+#        # TODO: Delte print
+#        for key, value in animationToggle.items():
+#            print(f"{key}:{value}")
+#        
+#        # loop through the actions
+#        for a in bpy.data.actions:
+#            # TODO: Delte print
+#            print("\na:",a.name)
+            
+        for animationKey, animation in animationFolderNames.items():
+            # Gets the action and sets it as current action
+            bpy.context.active_object.animation_data.action = bpy.data.actions.get(animation)
+            currentAction = bpy.context.active_object.animation_data.action
+            
+            if(animationToggle[animationKey] == True and currentAction != None):
+                # TODO: Delte print
+                print("\nanimation:",animation)
+                #assign the action
+                bpy.context.active_object.animation_data.action = bpy.data.actions.get(animation)
+                # TODO: Delte print
+                print("action:",bpy.context.active_object.animation_data.action)
+                # In this case there are no subframe(?!) animations, thus converted into integer.
+                # Furtehrmore this prevents rendering empty frames.
+                bpy.context.scene.frame_end = int(bpy.context.active_object.animation_data.action.frame_range[1])
+                # TODO: Delte print
+                print("\nFrame End:",bpy.context.scene.frame_end)
+                # Sets path of the sprites and subfolders
+                animation_folder = os.path.join(pathSaveAt, animation)
+                if not os.path.exists(animation_folder):
+                    os.makedirs(animation_folder)
+                # Uses only the camerapostions which are selected.    
+                for positionKey, position in cameraPosFolderNames.items():
+                    if(cameraPosToggles[positionKey] == True):
+                        # Create folder for specific angles
+                        position_folder = os.path.join(animation_folder, positionKey)                  
+                        if not os.path.exists(position_folder):
+                            os.makedirs(position_folder)
+                            
+                    # The script is not explicitly rotating the camera for each of the 8 directions. 
+                    # It's rotating the subject (the character/model) and relying on a static camera to capture it.
+                    bpy.context.active_object.rotation_euler[2] = math.radians(cameraAngles[positionKey])
+                    # TODO: Delte print
+                    print("\nbpy.context.active_object.rotation_euler:",bpy.context.active_object.rotation_euler)
+                    print("\nbpy.context.active_object.rotation_euler[2]:",bpy.context.active_object.rotation_euler[2])
+            
+            
+                    frameStart      = bpy.context.scene.frame_start
+                    frameEnd        = bpy.context.scene.frame_end
+                    frameCurrent    = bpy.context.scene.frame_current
+                    renderFrame     = bpy.ops.render.render
+                    Every_X_Frame   = context.scene.IEAS_properties.Every_X_Frame
+                    
+                    #loop through and render frames.  Can set how "often" it renders.
+                    for frame in range(frameStart,frameEnd,Every_X_Frame):
+                        frameCurrent = frame
+                        
+                        # TODO: Delte print
+                        print("position_folder:",position_folder)
+                        
+                        fileName = f"{prefixResref}{animationKey}_{positionKey}_{str(frameCurrent).zfill(5)}.png"
+                         # TODO: Delte print
+                        print("fileName:",fileName)
+                        
+                        bpy.context.scene.render.filepath = os.path.join(position_folder, fileName)
+                         # TODO: Delte print
+                        print("filepath:",bpy.context.scene.render.filepath)
+                        
+                        # TODO: Delte print
+                        print("action:",bpy.context.active_object.animation_data.action)
+                        
+                        renderFrame(  False, # This is the 'undo' parameter     
+                                      animation     =False, 
+                                      write_still   =True) 
         
         return {'FINISHED'}
 
@@ -226,6 +374,7 @@ class IEAS_PT_GlobalParameters(Panel):
         self.layout.prop(context.scene.IEAS_properties, "Save_at")
         self.layout.prop(context.scene.IEAS_properties, "Prefix")
         self.layout.prop(context.scene.IEAS_properties, "Resref")
+        self.layout.prop(context.scene.IEAS_properties, "Object_List")
         self.layout.prop(context.scene.IEAS_properties, "Resolution_X")
         self.layout.prop(context.scene.IEAS_properties, "Resolution_Y")
         self.layout.prop(context.scene.IEAS_properties, "Every_X_Frame")
@@ -345,7 +494,7 @@ class IEAS_PT_Animation(Panel):
         row.prop(context.scene.IEAS_properties, "Use_A3")
         
         row = self.layout.row()
-        row.prop(context.scene.IEAS_properties, "Attack1")
+        row.prop(context.scene.IEAS_properties, "Attack4")
         row.prop(context.scene.IEAS_properties, "Use_A4")
         
         row = self.layout.row()
